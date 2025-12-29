ab921e383d64e6feec3ab049ab7a613b2be1ccbb
Reda Haloubi <redahaloubi8@gmail.com>
2025-12-30 00:22:18 +0100

    V3

--- app/api/admin/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { auth } from "@/lib/auth";
-import { NextRequest } from "next/server";
-
-export async function POST(request: NextRequest) {
-	return await auth.api.signOut({
-		headers: request.headers,
-	});
-}
